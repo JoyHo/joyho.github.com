83b955bcf3f9a5baa5bf27b28afd4cfd5cafd5bb
JoyHo <yanzhuhe@gmail.com>
2013-08-05 19:33:37 +0800

    update all

--- js/worklist.js
+++ js/worklist.js
@@ -6,7 +6,8 @@
 			var this_src = $(this).find('img').eq(0).attr('src');
 			var x =parseInt(10+ e.pageX);
 			var y =parseInt(10+ e.pageY);
-			big_pic.html("<img src='"+this_src+"'style='width:500px;height:500px;' />");
+			var src_length =parseInt(this_src.length)-4;
+			big_pic.html("<img src='"+this_src.substring(0,src_length)+"_big"+this_src.substring(src_length)+"'style='width:500px;height:500px;' />");
 			big_pic.css({"display":"block","left":x+"px","top":y+"px"});
 			})
 			  .delegate('li','mouseout',function(){
